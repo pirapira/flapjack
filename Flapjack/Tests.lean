0cff6a9ab95c72bc5a6720972eada959cf580f1d
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-10 02:31:47 +0200

    Evaluate frame-machine LocValue with code

--- Flapjack/Tests.lean
+++ Flapjack/Tests.lean
@@ -32,6 +32,7 @@ import Flapjack.Test.LocValueAnalysis
 import Flapjack.Test.LocValueLowering
 import Flapjack.Test.LocValueWordLowering
 import Flapjack.Test.LocValueMachine
+import Flapjack.Test.LocValueFrameMachine
 import Flapjack.Test.StackRiscVDivision
 import Flapjack.Test.GraphLocations
 import Flapjack.Test.LinearScan

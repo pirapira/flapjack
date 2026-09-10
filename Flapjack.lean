32ea59fc7e1057ca4391f9033153508fa440e4a1
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-10 18:36:14 +0200

    Relate Store32 source and Crep states

--- Flapjack.lean
+++ Flapjack.lean
@@ -20,6 +20,7 @@ import Flapjack.CrepeRuntime
 import Flapjack.CrepeCorrectness
 import Flapjack.CrepeStateRelation
 import Flapjack.CrepeBasicControlRelation
+import Flapjack.CrepeStoreRelation
 import Flapjack.CrepeExpCorrectness
 import Flapjack.CrepeAssignmentCorrectness
 import Flapjack.CrepeSequenceCorrectness

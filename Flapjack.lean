8929a23545c0a0f5f00b30c27aba9145f12c86b9
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-11 01:48:14 +0200

    Revert "Add compositional source-word return correctness"
    This reverts commit 2c7ff1965431b45a9433c5a9c97f3c7d8f64553a.

--- Flapjack.lean
+++ Flapjack.lean
@@ -60,7 +60,6 @@ import Flapjack.CrepeProgramBasicRelation
 import Flapjack.CrepeProgramReturnRelation
 import Flapjack.CrepeProgramReturnFuelRelation
 import Flapjack.CrepeProgramSourceWordReturnRelation
-import Flapjack.CrepeProgramSourceWordReturnCorrectness
 import Flapjack.CrepeProgramAssignmentFuelRelation
 import Flapjack.CrepeProgramAssignmentCorrectness
 import Flapjack.CrepeProgramPrimitiveRelation

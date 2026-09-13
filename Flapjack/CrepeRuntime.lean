f9f85d482349d7c104a2d67286a70d726c431095
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-13 23:15:20 +0200

    fix Crepe FFI state update syntax

--- Flapjack/CrepeRuntime.lean
+++ Flapjack/CrepeRuntime.lean
@@ -273,7 +273,8 @@ def crepRuntimeSharedMem (handler : CrepRuntimeFfiHandler α σ ε)
         match handler (.sharedMem operator name address payload) state.ffi with
         | .returned ffi bytes =>
             let value := state.ffiContext.wordOfBytes false bytes
-            (.normal, { state with ffi := ffi
+            (.normal, { state with
+              ffi := ffi
               locals := updateCrepLocal state.locals name value })
         | .final event => (.finalFfi event, clearCrepRuntimeLocals state)
     | .store | .store8 | .store16 | .store32 =>

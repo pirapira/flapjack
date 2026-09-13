fb1cc88d02c883bba965931e7afde7f19701d0e1
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-13 23:30:58 +0200

    port loop_call comp parity

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -34,6 +34,7 @@ import Flapjack.Test.LoopAssignedVarsParity
 import Flapjack.Test.LoopAccVarsParity
 import Flapjack.Test.LoopNestedSeqParity
 import Flapjack.Test.LoopIsLoadParity
+import Flapjack.Test.LoopCallParity
 import Flapjack.Test.InstructionTransfer
 import Flapjack.Test.ArtifactFormat
 import Flapjack.Test.ParsedFullSsaPipeline
@@ -656,6 +657,7 @@ def main : IO Unit := do
     Flapjack.Test.LoopAccVarsParity.runChecks,
     Flapjack.Test.LoopNestedSeqParity.runChecks,
     Flapjack.Test.LoopIsLoadParity.runChecks,
+    Flapjack.Test.LoopCallParity.runChecks,
     Flapjack.Test.EndToEndParity.runChecks,
     Flapjack.Test.CakeStackReseatParity.runChecks
     ].mapM id
--- scripts/hol-probes/regenerate.sh
+++ scripts/hol-probes/regenerate.sh
@@ -114,6 +114,9 @@ run_probe loop_lang_nested_seq_probeScript.sml \
 run_probe loop_call_is_load_probeScript.sml \
   loop_call_is_load_probe.out \
   load store32 "$cake_dir/pancake/loop_callScript.sml"
+run_probe loop_call_comp_probeScript.sml \
+  loop_call_comp_probe.out \
+  skip fallback_keeps "$cake_dir/pancake/loop_callScript.sml"
 # The set_globals probe observes FLOOKUP after the original map update.
 run_probe loop_sem_set_globals_probeScript.sml loop_sem_set_globals_probe.out \
   set_globals_new set_globals_sibling "$cake_dir/pancake/semantics/loopSemScript.sml"

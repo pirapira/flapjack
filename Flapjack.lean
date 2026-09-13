2629ff7ac8b956d78570bbe325331cfadeeea524
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-13 12:21:28 +0200

    Export RISC-V trap contracts

--- Flapjack.lean
+++ Flapjack.lean
@@ -6,6 +6,7 @@ import Flapjack.PanStructs
 import Flapjack.PanGlobals
 import Flapjack.Pipeline
 import Flapjack.RiscV.PipelineDiagnostics
+import Flapjack.RiscV.CorrectnessTraps
 import Flapjack.Correctness
 import Flapjack.CorrectnessCalls
 import Flapjack.FlatCorrectness

aa76a3cb33a930b050751b5551c554e70edeee1d
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-13 22:56:29 +0200

    model empty Crepe FFI call identity

--- Flapjack/CrepeRuntime.lean
+++ Flapjack/CrepeRuntime.lean
@@ -233,12 +233,17 @@ def crepRuntimeExtCallValues [BEq α] [Add α] [OfNat α 1]
       crepRuntimeReadBytes state array
         (state.ffiContext.valueToNat arrayLength) with
   | some configurationBytes, some arrayBytes =>
-      match handler (.extCall function configurationBytes arrayBytes) state with
-      | .returned state bytes =>
-          match crepRuntimeWriteBytes state array bytes with
-          | some state => (.normal, state)
-          | none => (.error, state)
-      | .final event => (.finalFfi event, state)
+      if function == "" then
+        match crepRuntimeWriteBytes state array arrayBytes with
+        | some state => (.normal, state)
+        | none => (.error, state)
+      else
+        match handler (.extCall function configurationBytes arrayBytes) state with
+        | .returned state bytes =>
+            match crepRuntimeWriteBytes state array bytes with
+            | some state => (.normal, state)
+            | none => (.error, state)
+        | .final event => (.finalFfi event, state)
   | _, _ => (.error, state)
 
 def crepRuntimeExtCall [BEq α] [Add α] [OfNat α 1]

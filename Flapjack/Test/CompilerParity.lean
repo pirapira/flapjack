dae72b33a4ce58c9f6e969267f4e45e2c86bd99f
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-13 16:34:49 +0200

    Make Pancake probe regeneration incremental

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -7,6 +7,7 @@ import Flapjack.Test.LoopToWord
 import Flapjack.Test.PanMemoryParity
 import Flapjack.Test.PanShapeParity
 import Flapjack.Test.PanWordParity
+import Flapjack.Test.PanFixedLoadParity
 import Flapjack.Test.LoopGetVarsParity
 import Flapjack.Test.LoopSetGlobalsParity
 import Flapjack.Test.InstructionTransfer

552b10def947f1a41244d54747c59206df8b3433
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 04:49:38 +0200

    Port Crepe sh_mem_store

--- Flapjack.lean
+++ Flapjack.lean
@@ -26,6 +26,7 @@ import Flapjack.CrepEvaluate
 import Flapjack.CrepFixClock
 import Flapjack.CrepShMemLoad
 import Flapjack.CrepShMemOp
+import Flapjack.CrepShMemStore
 import Flapjack.CrepObservationalSemantics
 import Flapjack.PanBst
 import Flapjack.PanBStateUpdates

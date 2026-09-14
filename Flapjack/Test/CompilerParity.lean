5c477bcaa61bbcf343b26ba764f8bcdbfd63e28f
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 11:57:37 +0200

    Add pan globals fresh name parity

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -17,6 +17,7 @@ import Flapjack.Test.PanExpIdsParity
 import Flapjack.Test.PanWithShapeParity
 import Flapjack.Test.PanShapeValParity
 import Flapjack.Test.PanGlobalsCompileExpParity
+import Flapjack.Test.PanGlobalsFreshNameParity
 import Flapjack.Test.PanResVarParity
 import Flapjack.Test.PanPrimopParity
 import Flapjack.Test.PanSetVarParity

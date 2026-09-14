058605e0667285a51cc51951eb0563d315effc69
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 17:36:48 +0200

    Add Cake setup SSA and total colour parity

--- scripts/hol-probes/regenerate.sh
+++ scripts/hol-probes/regenerate.sh
@@ -127,6 +127,9 @@ run_probe mk_bij_probeScript.sml mk_bij_probe.out \
 run_probe reg_alloc_mk_bij_probeScript.sml reg_alloc_mk_bij_probe.out \
   empty_to seq_next "$cake_dir/compiler/backend/reg_alloc/reg_allocScript.sml" \
   "$cake_dir/compiler/backend"
+run_probe word_alloc_setup_colour_probeScript.sml word_alloc_setup_colour_probe.out \
+  total_colour_mapped_1 setup0_next "$cake_dir/compiler/backend/word_allocScript.sml" \
+  "$cake_dir/compiler/backend"
 run_probe pan_lang_shape_val_probeScript.sml pan_lang_shape_val_probe.out \
   one named "$cake_dir/pancake/panLangScript.sml"
 run_probe pan_res_var_probeScript.sml pan_res_var_probe.out \

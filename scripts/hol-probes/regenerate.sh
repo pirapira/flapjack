c3b3de3c8db5c7f7ef1e39e459eec2443ea0d4c1
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 06:02:59 +0200

    Port exact Pancake fixed-width load boundary

--- scripts/hol-probes/regenerate.sh
+++ scripts/hol-probes/regenerate.sh
@@ -161,6 +161,10 @@ run_probe pan_sem_is_valid_value_probeScript.sml pan_sem_is_valid_value_probe.ou
 run_probe pan_sem_write_bytearray_probeScript.sml pan_sem_write_bytearray_probe.out \
   write_empty write_miss \
   "$cake_dir/pancake/semantics/panSemScript.sml"
+run_probe pan_sem_evaluate_fixed_load_probeScript.sml \
+  pan_sem_evaluate_fixed_load_probe.out \
+  eval_byte_hit eval_load32_alignment_failure \
+  "$cake_dir/pancake/semantics/panSemScript.sml"
 run_probe pan_peg_try_default_probeScript.sml pan_peg_try_default_probe.out \
   try_default_success try_default_default \
   "$cake_dir/pancake/parser/panPEGScript.sml"

cb5992ce4e15a56c281673cc4588cac2013d1126
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 05:26:08 +0200

    Port Pancake write_bytearray

--- Flapjack.lean
+++ Flapjack.lean
@@ -18,6 +18,7 @@ import Flapjack.PanSemSetGlobal
 import Flapjack.PanSemSetKvar
 import Flapjack.PanSemLookupKvar
 import Flapjack.PanSemIsValidValue
+import Flapjack.PanSemWriteBytearray
 import Flapjack.PanItreeEvaluate
 import Flapjack.PanExt
 import Flapjack.PanHHandleCallRet
--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -56,6 +56,7 @@ import Flapjack.Test.PanSemSetGlobalParity
 import Flapjack.Test.PanSemSetKvarParity
 import Flapjack.Test.PanSemLookupKvarParity
 import Flapjack.Test.PanSemIsValidValueParity
+import Flapjack.Test.PanSemWriteBytearrayParity
 import Flapjack.Test.PanItreeEvaluateParity
 import Flapjack.Test.PanExtParity
 import Flapjack.Test.PanHHandleCallRetParity
@@ -747,6 +748,7 @@ def main : IO Unit := do
     Flapjack.Test.PanSemSetKvarParity.runChecks,
     Flapjack.Test.PanSemLookupKvarParity.runChecks,
     Flapjack.Test.PanSemIsValidValueParity.runChecks,
+    Flapjack.Test.PanSemWriteBytearrayParity.runChecks,
     Flapjack.Test.PanItreeEvaluateParity.runChecks,
     Flapjack.Test.PanExtParity.runChecks,
     Flapjack.Test.PanHHandleCallRetParity.runChecks,

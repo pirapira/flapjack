b0f5dfef0614ed338697dbdb5be2a025549d931d
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 14:13:22 +0200

    Add Crepe stores parity

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -100,6 +100,7 @@ import Flapjack.Test.CrepPrimopParity
 import Flapjack.Test.CrepeLoadShapeParity
 import Flapjack.Test.CrepeNestedSeqParity
 import Flapjack.Test.CrepAssignedFreeVarsParity
+import Flapjack.Test.CrepeStoresParity
 import Flapjack.Test.CrepProgIfParity
 import Flapjack.Test.CompileCrepOpParity
 import Flapjack.Test.CrepCompileExpParity
@@ -818,6 +819,7 @@ def main : IO Unit := do
     Flapjack.Test.CrepeLoadShapeParity.runChecks,
     Flapjack.Test.CrepeNestedSeqParity.runChecks,
     Flapjack.Test.CrepAssignedFreeVarsParity.runChecks,
+    Flapjack.Test.CrepeStoresParity.runChecks,
     Flapjack.Test.CrepExitLoopParity.runChecks,
     Flapjack.Test.CrepEvaluateParity.runChecks,
     Flapjack.Test.CrepFixClockParity.runChecks,
--- scripts/hol-probes/regenerate.sh
+++ scripts/hol-probes/regenerate.sh
@@ -235,6 +235,8 @@ run_probe crep_nested_seq_probeScript.sml crep_nested_seq_probe.out \
   empty assign_seq "$cake_dir/pancake/crepLangScript.sml"
 run_probe crep_assigned_free_vars_probeScript.sml crep_assigned_free_vars_probe.out \
   skip shmem_fallback "$cake_dir/pancake/crepLangScript.sml"
+run_probe crep_stores_probeScript.sml crep_stores_probe.out \
+  empty nonzero_two "$cake_dir/pancake/crepLangScript.sml"
 run_probe crep_exit_loop_probeScript.sml crep_exit_loop_probe.out \
   exit_loop_break exit_loop_error \
   "$cake_dir/pancake/semantics/crepSemScript.sml"

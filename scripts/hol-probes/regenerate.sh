5fd1e05caff9c9f78f05f8ffcdd7c6d7294066dd
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 18:46:54 +0200

    Regenerate every allocator ordering oracle case

--- scripts/hol-probes/regenerate.sh
+++ scripts/hol-probes/regenerate.sh
@@ -150,7 +150,7 @@ run_probe reg_alloc_probeScript.sml reg_alloc_probe.out \
   "$cake_dir/compiler/backend/reg_alloc/reg_allocScript.sml" \
   "$cake_dir/compiler/backend"
 run_probe sort_moves_probeScript.sml sort_moves_probe.out \
-  sm_ties_two ra_moves_stemp_hi \
+  sm_ties_two sm_ties_three sm_desc ra_moves_stemp ra_moves_stemp_hi \
   "$cake_dir/compiler/backend/reg_alloc/reg_allocScript.sml" \
   "$cake_dir/compiler/backend"
 run_probe pan_lang_shape_val_probeScript.sml pan_lang_shape_val_probe.out \

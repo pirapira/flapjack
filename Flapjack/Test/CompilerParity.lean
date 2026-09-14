2b17adb610f934cb7ea1d680fc1f576c36bf57b9
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 04:25:11 +0200

    Port Crepe observational semantics

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -61,6 +61,7 @@ import Flapjack.Test.PanHProgCallParity
 import Flapjack.Test.CrepPrimopParity
 import Flapjack.Test.CrepExitLoopParity
 import Flapjack.Test.CrepEvaluateParity
+import Flapjack.Test.CrepObservationalSemanticsParity
 import Flapjack.Test.PanBstParity
 import Flapjack.Test.PanBStateUpdatesParity
 import Flapjack.Test.PanEmptyLocalsParity
@@ -732,6 +733,7 @@ def main : IO Unit := do
     Flapjack.Test.CrepPrimopParity.runChecks,
     Flapjack.Test.CrepExitLoopParity.runChecks,
     Flapjack.Test.CrepEvaluateParity.runChecks,
+    Flapjack.Test.CrepObservationalSemanticsParity.runChecks,
     Flapjack.Test.PanBstParity.runChecks,
     Flapjack.Test.PanBStateUpdatesParity.runChecks,
     Flapjack.Test.PanEmptyLocalsParity.runChecks,

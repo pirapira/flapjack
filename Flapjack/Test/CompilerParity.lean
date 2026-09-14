58ce333e0ddbb996815d25991f15a25f8a3a8716
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 14:51:20 +0200

    Add Crepe exps parity

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -107,6 +107,7 @@ import Flapjack.Test.CrepeStoreGlobalsParity
 import Flapjack.Test.CrepeLoadGlobalsParity
 import Flapjack.Test.CrepeAssignRetParity
 import Flapjack.Test.CrepeVarCexpParity
+import Flapjack.Test.CrepExpsParity
 import Flapjack.Test.CrepProgIfParity
 import Flapjack.Test.CompileCrepOpParity
 import Flapjack.Test.CrepCompileExpParity
@@ -832,6 +833,7 @@ def main : IO Unit := do
     Flapjack.Test.CrepeLoadGlobalsParity.runChecks,
     Flapjack.Test.CrepeAssignRetParity.runChecks,
     Flapjack.Test.CrepeVarCexpParity.runChecks,
+    Flapjack.Test.CrepExpsParity.runChecks,
     Flapjack.Test.CrepExitLoopParity.runChecks,
     Flapjack.Test.CrepEvaluateParity.runChecks,
     Flapjack.Test.CrepFixClockParity.runChecks,

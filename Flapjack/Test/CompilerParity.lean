35dbef97fb9f1c01b99770f483a52417bd95230c
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 12:17:18 +0200

    Add pan globals fperm decs parity

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -23,6 +23,7 @@ import Flapjack.Test.PanGlobalsCompileDecsParity
 import Flapjack.Test.PanGlobalsResortDeclsParity
 import Flapjack.Test.PanGlobalsFpermNameParity
 import Flapjack.Test.PanGlobalsFpermParity
+import Flapjack.Test.PanGlobalsFpermDecsParity
 import Flapjack.Test.PanResVarParity
 import Flapjack.Test.PanPrimopParity
 import Flapjack.Test.PanSetVarParity

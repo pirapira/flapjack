2a1baac661eb64a102b35d62fb0e51fef846b47b
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-14 14:18:57 +0200

    Add Crepe store_globals parity

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -102,6 +102,7 @@ import Flapjack.Test.CrepeNestedSeqParity
 import Flapjack.Test.CrepAssignedFreeVarsParity
 import Flapjack.Test.CrepeStoresParity
 import Flapjack.Test.CrepeNestedDecsParity
+import Flapjack.Test.CrepeStoreGlobalsParity
 import Flapjack.Test.CrepProgIfParity
 import Flapjack.Test.CompileCrepOpParity
 import Flapjack.Test.CrepCompileExpParity
@@ -822,6 +823,7 @@ def main : IO Unit := do
     Flapjack.Test.CrepAssignedFreeVarsParity.runChecks,
     Flapjack.Test.CrepeStoresParity.runChecks,
     Flapjack.Test.CrepeNestedDecsParity.runChecks,
+    Flapjack.Test.CrepeStoreGlobalsParity.runChecks,
     Flapjack.Test.CrepExitLoopParity.runChecks,
     Flapjack.Test.CrepEvaluateParity.runChecks,
     Flapjack.Test.CrepFixClockParity.runChecks,

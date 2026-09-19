53a3f867d6882a9bf07c00fe31c0ffcbd686e208
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-19 22:55:56 +0200

    Port Cake primop_to_str

--- Flapjack/Test/LoopCompiler.lean
+++ Flapjack/Test/LoopCompiler.lean
@@ -213,6 +213,7 @@ def reachabilityContext : Context :=
   binopToString .or == "Or" &&
   binopToString .xor == "Xor"
 #guard panopToString .mul == "Mul"
+#guard primopToString .addCarry == "AddCarry"
 #guard
   addPrimitiveHint "__add_with_carry__" "error: unsupported primitive\n" ==
     "error: unsupported primitive\n  note: __add_with_carry__ is a built-in primitive only available in declaration or assignment RHS positions\n" &&

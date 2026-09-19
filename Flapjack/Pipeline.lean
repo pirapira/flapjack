14deef6b43be6d29c2b3f5490108d25f90135a22
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-19 03:42:40 +0200

    Use Cake loop-live output in RISC-V source paths
    (cherry picked from commit e2e8565739428b56a6cb424bfc3d97d50b343dca)

--- Flapjack/Pipeline.lean
+++ Flapjack/Pipeline.lean
@@ -874,7 +874,7 @@ def compileFlapjackRiscVViaAllocatedStackWithFullSsaAndBitmapsAndSimpleGcTarget
     (declarations : List (Decl (RiscV.Word width))) :
     Option (RiscV.WordStackBitmapState × List (RiscV.Instruction width)) := do
   let pipeline := compileFlapjackTarget architecture bytesInWord fromNat declarations
-  let loop := pipelineLoopFunctions architecture stackFunctionFirstLabel pipeline.crepe
+  let loop := pipeline.loop
   let (functions, bitmaps) ←
     pipelineWordFunctionsAllocatedWithSpillsAndFullSsaAndBitmaps
       (RiscV.wordStackInitialBitmaps false) loop
@@ -904,7 +904,7 @@ def compileFlapjackRiscVViaAllocatedStackWithFullSsaAndBitmapsAndSimpleGcTargetL
     Option (RiscV.WordStackBitmapState ×
       List (Nat × RiscV.Word width × List (RiscV.Instruction width))) := do
   let pipeline := compileFlapjackTarget architecture bytesInWord fromNat declarations
-  let loop := pipelineLoopFunctions architecture stackFunctionFirstLabel pipeline.crepe
+  let loop := pipeline.loop
   let (functions, bitmaps) ←
     pipelineWordFunctionsAllocatedWithSpillsAndFullSsaAndBitmaps
       (RiscV.wordStackInitialBitmaps false) loop
@@ -935,7 +935,7 @@ def compileFlapjackRiscVViaAllocatedStackWithFullSsaAndBitmapsAndSimpleGcEntryLi
     Option (RiscV.WordStackBitmapState ×
       List (Nat × RiscV.Word width × List (RiscV.Instruction width))) := do
   let pipeline ← compileFlapjackEntry architecture bytesInWord fromNat start declarations
-  let loop := pipelineLoopFunctions architecture stackFunctionFirstLabel pipeline.crepe
+  let loop := pipeline.loop
   let (functions, bitmaps) ←
     pipelineWordFunctionsAllocatedWithSpillsAndFullSsaAndBitmaps
       (RiscV.wordStackInitialBitmaps false) loop

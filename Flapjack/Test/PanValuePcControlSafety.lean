828acee85f510fbc1f63a9af6b4a57e0498ae57f
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-21 18:50:52 +0200

    Restore ordinary DecCall return projection

--- Flapjack/Test/PanValuePcControlSafety.lean
+++ Flapjack/Test/PanValuePcControlSafety.lean
@@ -18,6 +18,7 @@ import Flapjack.PanToCrepAnnotCorrectness
 import Flapjack.PanValueFfiClockCorrectness
 import Flapjack.CrepeNestedDecsStability
 import Flapjack.CrepeRaisedCallInversion
+import Flapjack.PanToCrepDecCallCorrectness
 import Flapjack.PanToCrepProgramComposition
 
 namespace Flapjack.Test.PanValuePcControlSafety

939824b61e4e2f754b054fc346b3e92cafc97ac0
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-22 09:02:15 +0200

    Add final FFI semantic outcome bridge

--- Flapjack/Test/PanToCrepSemantics.lean
+++ Flapjack/Test/PanToCrepSemantics.lean
@@ -12,6 +12,7 @@ open Flapjack
 #check @Flapjack.PanValuePcSemanticClockEvidence.crossResultRel
 #check @Flapjack.PanValuePcSemanticClockEvidence.crossResultRel_withContextCode
 #check @Flapjack.PanValuePcSemanticClockEvidence.returnedSemanticOutcomeRel
+#check @Flapjack.PanValuePcSemanticClockEvidence.finalFfiSemanticOutcomeRel
 #check @Flapjack.panCrepSemanticAgreement_of_pcCompileCorrect
 #check @Flapjack.panCrepSemanticAgreement_of_pcCompileCorrect_cross_clock
 #check @Flapjack.panEventPrefix_antisymm

96a8c23d287fbf869422079e8ed5d2ebaf55e5d9
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-24 06:36:23 +0200

    Use full evaluator for PanStructs LoadByte case

--- scripts/hol-probes/regenerate.sh
+++ scripts/hol-probes/regenerate.sh
@@ -116,6 +116,7 @@ run_probe pan_structs_compile_exp_correct_probeScript.sml pan_structs_compile_ex
   compile_exp_correct_rfield compile_exp_correct_op compile_exp_correct_load \
   compile_exp_correct_load_nested_named \
   compile_exp_correct_load32_le_success \
+  compile_exp_correct_load_byte_out_of_domain \
   size_of_compile_shape_comb \
   "$cake_dir/pancake/proofs/pan_structsProofScript.sml" \
   "$cake_dir/pancake/proofs"

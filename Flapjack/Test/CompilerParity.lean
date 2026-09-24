9cb0bde0a2e7c714a1f4bd2c55e1578c32e2c1a6
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-24 15:41:27 +0200

    Port wordConvs pre_alloc_conventions and post_alloc_conventions

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -228,6 +228,7 @@ import Flapjack.Test.WordLangEveryVarParity
 import Flapjack.Test.WordLangGoodHandlersParity
 import Flapjack.Test.NumSetAuditParity
 import Flapjack.Test.WordLangEveryNameParity
+import Flapjack.Test.WordLangAllocConventionsParity
 import Flapjack.Test.AsmConfigChecksParity
 import Flapjack.Test.PanSemDecCallErrorParity
 import Flapjack.Test.PanObservationalSemanticsParity
@@ -838,6 +839,7 @@ Flapjack.Test.PanSemFuelDecompositionParity.runChecks,
     Flapjack.Test.WordLangGoodHandlersParity.runChecks,
     Flapjack.Test.NumSetAuditParity.runChecks,
     Flapjack.Test.WordLangEveryNameParity.runChecks,
+    Flapjack.Test.WordLangAllocConventionsParity.runChecks,
     Flapjack.Test.AsmConfigChecksParity.runChecks,
     Flapjack.Test.PanSemDecCallErrorParity.runChecks,
     Flapjack.Test.PanObservationalSemanticsParity.runChecks,

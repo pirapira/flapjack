ec9136dae35effa2e79ae229212f77d2c3f44897
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-24 21:52:00 +0200

    Drop stale WordToStackBitsParity registration and add Cake bits bridge parity test

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -233,9 +233,9 @@ import Flapjack.Test.WordLangEveryNameParity
 import Flapjack.Test.WordLangAllocConventionsParity
 import Flapjack.Test.LabPropsLineOkPreParity
 import Flapjack.Test.LabPropsSecEndsLabelParity
+import Flapjack.Test.CakeAllocatorBitsBridgeParity
 import Flapjack.Test.RiscvConfigParity
 import Flapjack.Test.MiscAppListParity
-import Flapjack.Test.WordToStackBitsParity
 import Flapjack.Test.StackToLabFlattenOpsParity
 import Flapjack.Test.StackToLabFlattenBaseParity
 import Flapjack.Test.StackToLabFlattenAppParity
@@ -852,9 +852,9 @@ Flapjack.Test.PanSemFuelDecompositionParity.runChecks,
     Flapjack.Test.WordLangAllocConventionsParity.runChecks,
     Flapjack.Test.LabPropsLineOkPreParity.runChecks,
     Flapjack.Test.LabPropsSecEndsLabelParity.runChecks,
+    Flapjack.Test.CakeAllocatorBitsBridgeParity.runChecks,
     Flapjack.Test.RiscvConfigParity.runChecks,
     Flapjack.Test.MiscAppListParity.runChecks,
-    Flapjack.Test.WordToStackBitsParity.runChecks,
     Flapjack.Test.StackToLabFlattenOpsParity.runChecks,
     Flapjack.Test.StackToLabFlattenBaseParity.runChecks,
     Flapjack.Test.StackToLabFlattenAppParity.runChecks,

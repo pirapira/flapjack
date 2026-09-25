03477dab39376335f5cdca321f2ed090c91f9002
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-25 03:18:53 +0200

    Add byte-rangedness foundation lemmas for the Pancake parser (bead 18.3.5.8.7.1)

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -279,6 +279,7 @@ import Flapjack.Test.ParserTryProgParity
 import Flapjack.Test.ParserPancakePegParity
 import Flapjack.Test.ParserParseStatementParity
 import Flapjack.Test.ParserByteNamesParity
+import Flapjack.Test.ParserByteRangedParity
 import Flapjack.Test.PanLangWfShapeParity
 import Flapjack.Test.PanLangWfFieldsContextParity
 import Flapjack.Test.PanHHandleCallRetParity

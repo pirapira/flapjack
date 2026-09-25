bc1d0e2cbfc86204fafa28b2d0b321bb1cc550c2
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-25 05:29:19 +0200

    Port exact HOL panSem result carrier and Dec-with-initializer clause

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -223,6 +223,7 @@ import Flapjack.Test.PanSemValueHOLParity
 import Flapjack.Test.PanSemStateExactParity
 import Flapjack.Test.PanSemLocalUpdatesExactParity
 import Flapjack.Test.PanSemIsValidValueExactParity
+import Flapjack.Test.PanSemDecExactParity
 import Flapjack.Test.PanSemIteParity
 import Flapjack.Test.PanSemAssignMemoryParity
 import Flapjack.Test.PanSemReturnRaiseExactParity
@@ -869,6 +870,7 @@ def main : IO Unit := do
     Flapjack.Test.PanSemStateExactParity.runChecks,
     Flapjack.Test.PanSemLocalUpdatesExactParity.runChecks,
     Flapjack.Test.PanSemIsValidValueExactParity.runChecks,
+    Flapjack.Test.PanSemDecExactParity.runChecks,
     Flapjack.Test.PanSemIteParity.runChecks,
     Flapjack.Test.PanSemAssignMemoryParity.runChecks,
 Flapjack.Test.PanSemReturnRaiseExactParity.runChecks,

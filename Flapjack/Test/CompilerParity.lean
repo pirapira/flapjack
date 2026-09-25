81421feb93c8077413b08e6eb5e451ee8c2a281b
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-25 05:56:47 +0200

    Withdraw duplicate ResultHOL in favour of integrated PanSemResultExact

--- Flapjack/Test/CompilerParity.lean
+++ Flapjack/Test/CompilerParity.lean
@@ -361,7 +361,6 @@ import Flapjack.Test.PanLangIsWfShapeParity
 import Flapjack.Test.PanSemIsValWordHOLParity
 import Flapjack.Test.PanSemEmptyLocalsHOLParity
 import Flapjack.Test.PanSemMemStore32HOLParity
-import Flapjack.Test.PanSemResultHOLParity
 import Flapjack.Test.PanSemMemLoadExactParity
 
 /-!
@@ -991,7 +990,6 @@ Flapjack.Test.PanSemFuelDecompositionParity.runChecks,
     Flapjack.Test.PanSemIsValWordHOLParity.runChecks,
     Flapjack.Test.PanSemEmptyLocalsHOLParity.runChecks,
     Flapjack.Test.PanSemMemStore32HOLParity.runChecks,
-    Flapjack.Test.PanSemResultHOLParity.runChecks,
     Flapjack.Test.PanSemMemLoadExactParity.runChecks,
     Flapjack.Test.CrepSemTotalExtCallParity.runChecks
     ].mapM id

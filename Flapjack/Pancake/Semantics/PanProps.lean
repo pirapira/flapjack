ee0a8a6b3cb17f5491109b97f7cc1d848cb8b60e
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-09-26 13:30:34 +0200

    Reuse existing PanProps finite state for structs_simps

--- Flapjack/Pancake/Semantics/PanProps.lean
+++ Flapjack/Pancake/Semantics/PanProps.lean
@@ -7,7 +7,6 @@ import Flapjack.Pancake.Semantics.PanSem.LocalUpdatesExact
 import Flapjack.Pancake.Semantics.PanSem.MemLoadHOL
 import Flapjack.Pancake.Semantics.PanSem.DeclContextExact
 import Flapjack.Pancake.Semantics.PanSem.EvalExact
-import Flapjack.Pancake.Semantics.PanProps.StructsSimps
 
 /-!
 HOL counterpart module for `cakeml/pancake/semantics/panPropsScript.sml`.
